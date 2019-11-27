93d0148a36f861fd84c56aa6421846f7b92b2183
Gaurav Kesarwani <kesarwani@verizonmedia.com>
2019-11-26 20:32:41 -0800

    deleted the url checker file

--- src/client/js/urlChecker.js
+++
@@ -1,9 +0,0 @@
-const validUrl = require('valid-url');
-
-function validateUrl(url) {
-    console.log("::: Running URL validation  :::", url);
-
-    return validUrl.isUri(url);
-}
-
-export { validateUrl }
